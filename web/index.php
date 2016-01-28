4addcd98299a9c749244ec03db712d9eaef6f81a
jtonynet <jtony.net@gmail.com>
2016-01-28 15:46:53 -0200

    htest

--- web/index.php
+++ web/index.php
@@ -8,16 +8,12 @@ error_reporting(-1);
 setlocale(LC_ALL, 'pt_BR.UTF-8', 'pt_BR', 'portuguese');
 date_default_timezone_set( "America/Sao_Paulo" );
 
+echo "teste";
+exit;
+
 $loader = require_once __DIR__.'/../vendor/autoload.php';
 
 $app = require __DIR__ . '/../src/app.php';
 require __DIR__ . '/../src/routes.php';
-
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
-});
 
 $app->run();
